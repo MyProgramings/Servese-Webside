6698279d844e3930d9dd0bbee1941aadd143166f
Abdullah <Abdullah@DESKTOP-VMU2MBJ>
2021-09-10 05:33:00 +1400

    Add project files.

---
+++ Servese Webside/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Servese_Webside.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
